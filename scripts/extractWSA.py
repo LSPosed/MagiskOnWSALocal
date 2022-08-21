29d55d7a655485ec3376bdbd8d6f7f7dabcbb5cf
Alessandro <59087068+alex-sandro92@users.noreply.github.com>
2022-08-21 22:30:32 +0200

    Update extractWSA.py

--- scripts/extractWSA.py
+++ scripts/extractWSA.py
@@ -24,9 +24,11 @@ with zipfile.ZipFile(Path.cwd().parent / "download/wsa.zip") as zip:
     for f in zip.filelist:
         if arch in f.filename.lower():
             zip_name = f.filename
+            output_name = zip_name[11:-5]
+            print(output_name)
             if not os.path.isfile(workdir / zip_name):
                 zip_path = workdir / zip_name
-                print(f"unzipping to {workdir}", flush=True)
+                #print(f"unzipping to {workdir}", flush=True)
                 zip.extract(f, workdir)
                 ver_no = zip_name.split("_")
                 long_ver = ver_no[1]
@@ -53,7 +55,7 @@ with zipfile.ZipFile(Path.cwd().parent / "download/wsa.zip") as zip:
                         l.extract(g, workdir / 'xml')
 with zipfile.ZipFile(zip_path) as zip:
     if not os.path.isdir(workdir / arch):
-        print(f"unzipping from {zip_path}", flush=True)
+        #print(f"unzipping from {zip_path}", flush=True)
         zip.extractall(workdir / arch)
 
-print("done", flush=True)
+#print("done", flush=True)
