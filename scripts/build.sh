530d4c574ec31583e5c8a21e06ba7b1b21fd7513
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2023-03-29 20:08:32 +0800

    Fix typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -142,7 +142,7 @@ vhdx_to_img() {
 
 # workaround for Debian
 # In Debian /usr/sbin is not in PATH and some utilities are not in /bin
-[ -f /usr/sbin ] && export PATH="/usr/sbin:$PATH"
+[ -d /usr/sbin ] && export PATH="/usr/sbin:$PATH"
 # In Debian /etc/mtab is not exist
 [ -f /etc/mtab ] || ln -s /proc/self/mounts /etc/mtab
 
