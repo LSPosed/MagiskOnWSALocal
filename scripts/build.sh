e5542d9e2e39a2d9877f3384136fefc249e1f4e2
Howard Wu <HowardWu20@outlook.com>
2023-04-06 02:49:09 +0800

    Allow stable Magisk

--- scripts/build.sh
+++ scripts/build.sh
@@ -494,7 +494,7 @@ if [ "$GAPPS_BRAND" != "none" ] || [ "$ROOT_SOL" = "magisk" ]; then
         fi
         # shellcheck disable=SC1090
         source "$WSA_WORK_ENV" || abort
-        if [ "$MAGISK_VERSION_CODE" -lt 25211 ]; then
+        if [ "$MAGISK_VERSION_CODE" -lt 25211 ] && [ "$MAGISK_VER" != "stable" ] && [ -z ${CUSTOM_MAGISK+x} ]; then
             echo "Please install Magisk 25211+"
             abort
         fi
