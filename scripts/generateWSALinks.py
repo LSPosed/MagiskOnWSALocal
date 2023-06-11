2c962a5419444875100eb7cb153834bb0f80c997
Doddddd <88972478+Doddddd@users.noreply.github.com>
2023-06-11 23:26:31 +0800

    Update generateWSALinks.py

--- scripts/generateWSALinks.py
+++ scripts/generateWSALinks.py
@@ -60,6 +60,7 @@ download_dir = Path.cwd().parent / \
     "download" if sys.argv[3] == "" else Path(sys.argv[3])
 ms_account_conf = download_dir/".ms_account"
 tempScript = sys.argv[4]
+skip_wsa_download = sys.argv[5] == "no" if len(sys.argv) >= 6 else False
 cat_id = '858014f3-3934-4abe-8078-4aa193e74ca8'
 user = ''
 session = Session()
@@ -153,7 +154,7 @@ for filename, values in identities.items():
     elif re.match(f"Microsoft\.VCLibs\..+_.*_{arch}_.*\.appx", filename):
         out_file_name = f"{values[1]}_{arch}.appx"
         out_file = download_dir / out_file_name
-    elif re.match(f"MicrosoftCorporationII\.WindowsSubsystemForAndroid_.*\.msixbundle", filename):
+    elif not skip_wsa_download and re.match(f"MicrosoftCorporationII\.WindowsSubsystemForAndroid_.*\.msixbundle", filename):
         wsa_long_ver = re.search(u'\d{4}.\d{5}.\d{1,}.\d{1,}', filename).group()
         print(f'WSA Version={wsa_long_ver}\n', flush=True)
         main_ver = wsa_long_ver.split(".")[0]
