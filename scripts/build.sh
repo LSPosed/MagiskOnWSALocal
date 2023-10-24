0c4c7521f7ccf010a7a39d75e39b3f343dd7c2f2
sn-o-w <cristian.silaghi@mozilla.ro>
2023-10-24 16:38:50 +0300

    Download Microsoft component packages while using `--skip-download-wsa` (#698)
    Co-authored-by: Doddddd <88972478+Doddddd@users.noreply.github.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -509,6 +509,8 @@ if [ -z ${OFFLINE+x} ]; then
         # shellcheck disable=SC1090
         source "$WSA_WORK_ENV" || abort
     else
+        echo "Generate Download Links"
+        python3 generateWSALinks.py "$ARCH" "$RELEASE_TYPE" "$DOWNLOAD_DIR" "$DOWNLOAD_CONF_NAME" "$DOWN_WSA" || abort
         WSA_MAJOR_VER=$(python3 getWSAMajorVersion.py "$ARCH" "$WSA_ZIP_PATH")
     fi
     if [[ "$WSA_MAJOR_VER" -lt 2211 ]]; then
