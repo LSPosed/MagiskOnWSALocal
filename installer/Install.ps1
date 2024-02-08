85db04bdc8659e9f8b1124c2e944a44d007b66dc
Howard Wu <HowardWu20@outlook.com>
2024-02-08 18:28:45 +0800

    2024

--- installer/Install.ps1
+++ installer/Install.ps1
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with MagiskOnWSALocal.  If not, see <https://www.gnu.org/licenses/>.
 #
-# Copyright (C) 2023 LSPosed Contributors
+# Copyright (C) 2024 LSPosed Contributors
 #
 
 $Host.UI.RawUI.WindowTitle = "Installing MagiskOnWSA...."
