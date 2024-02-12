e3130bd0637a926fc86cb5dcb070103d4169556f
Howard Wu <HowardWu20@outlook.com>
2024-02-12 09:01:25 +0800

    Refine post-fs-data.sh

--- scripts/post-fs-data.sh
+++ scripts/post-fs-data.sh
@@ -6,24 +6,13 @@ if [ ! -d /data/adb ]; then
     mkdir -m 700 /data/adb
     chcon u:object_r:adb_data_file:s0 /data/adb
 fi
-if [ ! -x $MAGISKBIN/busybox ]; then
+if [ ! -d $MAGISKBIN ]; then
     # shellcheck disable=SC2174
     mkdir -p -m 755 $MAGISKBIN
     chcon u:object_r:system_file:s0 $MAGISKBIN
-    ABI=$(/system/bin/getprop ro.product.cpu.abi)
-    /system/bin/unzip -d $MAGISKBIN -j $MAGISKTMP/stub.apk "lib/$ABI/libbusybox.so"
-    mv $MAGISKBIN/libbusybox.so $MAGISKBIN/busybox
-    chmod 755 $MAGISKBIN/busybox
-    /system/bin/unzip -d $MAGISKBIN -j $MAGISKTMP/stub.apk "lib/$ABI/libmagiskpolicy.so"
-    mv $MAGISKBIN/libmagiskpolicy.so $MAGISKBIN/magiskpolicy
-    chmod 755 $MAGISKBIN/magiskpolicy
 fi
-if [ ! -x $MAGISKBIN/magiskpolicy ]; then
-    /system/bin/unzip -d $MAGISKBIN -j $MAGISKTMP/stub.apk "lib/$ABI/libmagiskpolicy.so"
-    mv $MAGISKBIN/libmagiskpolicy.so $MAGISKBIN/magiskpolicy
-    chmod 755 $MAGISKBIN/magiskpolicy
-fi
-for file in magiskboot magiskinit; do
+ABI=$(/system/bin/getprop ro.product.cpu.abi)
+for file in busybox magiskpolicy magiskboot magiskinit; do
     [ -x "$MAGISKBIN/$file" ] || {
         /system/bin/unzip -d $MAGISKBIN -j $MAGISKTMP/stub.apk "lib/$ABI/lib$file.so"
         mv $MAGISKBIN/lib$file.so $MAGISKBIN/$file
